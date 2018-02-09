86640c80b9e01fae5d45e72ddcae32a890e4e3ac
stephanie wu <stephanie.wu@wework.com>
2018-02-09 01:41:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 board = " ", " ", " ", " ", " ", " ", " ", " ", " " 
 
 def display_board(board)
-  puts "   #{board[0]}   |   #{board[1]}   |   #{board[2]}   "
+  puts [#{board[0]}", "  |   #{board[1]}   |   #{board[2]}   "
   puts "____________"
   puts "   #{board[3]}   |   #{board[4]}   |   #{board[5]}   "
   puts "____________"
