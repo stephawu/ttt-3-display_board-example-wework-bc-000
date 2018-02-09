732c0799abbdb515c3cb2a05fbdd17be1cb3e2ff
stephanie wu <stephanie.wu@wework.com>
2018-02-09 01:37:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@ spaces = " ", " ", " ", " ", " ", " ", " ", " ", " "
 
 def display_board(board)
   puts "   #{board[0]}   |   #{board[1]}   |   #{board[2]}   "
-  puts "_____________"
+  puts "___________"
   puts "   #{board[3]}   |   #{board[4]}   |   #{board[5]}   "
-  puts "_____________"
+  puts "___________"
   puts "   #{board[6]}   |   #{board[7]}   |   #{board[8]} "
 end
 
