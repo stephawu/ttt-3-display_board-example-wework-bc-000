79c346a182d5b7fdd71efd5c4c75d47195f006e3
stephanie wu <stephanie.wu@wework.com>
2018-02-09 01:39:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-spaces = " ", " ", " ", " ", " ", " ", " ", " ", " " 
+board = " ", " ", " ", " ", " ", " ", " ", " ", " " 
 
 def display_board(board)
   puts "   #{board[0]}   |   #{board[1]}   |   #{board[2]}   "
